9873a4271e6adf8ac6d2a34cc07ba0c421bd929b
Daniel Dolezal <d.dolezal97@protonmail.com>
2023-04-30 15:40:18 +0200

    update build script

--- build.sh
+++ build.sh
@@ -1,48 +1,27 @@
 #!/bin/bash
+source "../build-functions.sh"
+source "../build-config.sh"
 
 DOCKER_IMAGE_NAME="daniel156161/apache-php"
-DOCKER_CONTAINER_NAME="apache"
-GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-run_docker_container() {
+function run_docker_container {
   echo "Running..."
   docker run -dp 80:80 \
     -e UID=$(id -u) \
     -e GID=$(id -g) \
-    -v "$PWD"/moodle:/var/www/html/ \
-    moodlehq/moodle-php-apache:7.1
+    -v "$PWD":/var/www/html/ \
+    "$DOCKER_IMAGE_NAME:$GIT_BRANCH"
 }
-
-build_docker_image() {
-  TAG="$1"
-
-  echo "Building..."
-  docker buildx build --push \
-    --platform linux/amd64 \
-    --tag "$DOCKER_IMAGE_NAME:$TAG" .
-}
-
-if [ "$GIT_BRANCH" == "master" ]; then
-  GIT_BRANCH="latest"
-fi
 
 case "$1" in
   run)
     run_docker_container
     ;;
   build)
-    build_docker_image "$GIT_BRANCH"
-    ;;
-  upload)
-    build_docker_image "$GIT_BRANCH"
-    docker push "$DOCKER_IMAGE_NAME:$GIT_BRANCH"
-    ;;
-  test)
-    build_docker_image "$GIT_BRANCH"
-    run_docker_container
+    build_docker_image "$DOCKER_IMAGE_NAME:$GIT_BRANCH"
     ;;
   *)
-    echo "Usage: $0 {run|build|test|upload}"
+    echo "Usage: $0 {run|build}"
     exit 1
     ;;
 esac
